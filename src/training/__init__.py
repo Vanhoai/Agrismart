047707cf3cb9b382ea72e996668ee2b127214e03
Hinsun <vanhoai.adv@gmail.com>
2025-07-15 20:43:24 +0700

    feat: add new dependencies for image processing and model tracking, update .gitignore, and remove unused YOLO model

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,8 +1,20 @@
-from ultralytics import YOLO
+import supervision as sv
+from ultralytics import YOLO, SAM
+import cv2
 
-source = "/Users/hinsun/Workspace/Software/Agrismart/assets/traffic.mp4"
+# source_path = "/Users/hinsun/Workspace/Software/Agrismart/assets/traffic.mp4"
+image_path = "/Users/hinsun/Workspace/Software/Agrismart/assets/traffic.jpg"
 
 def main() -> None:
-    # Load a pretrained YOLO model (recommended for training)
-    model = YOLO("yolo11n.pt")
-    results = model.track(source=source, show=True)
+    # (2160, 3840, 3)
+    # model = YOLO("yolo11x.pt")
+
+    # model.info()
+    # model.predict(source=image_path, imgsz=3840, conf=0.25, show=True, save=True)
+
+    model = SAM("sam2.1_b.pt")
+    results = model(image_path, points=[[992, 1630], [1379, 1325], [1397, 1200], [1450, 1024], [1334, 883], [1118, 667], [1055, 707], [844, 634], [725, 601], [1015, 558], [401, 498], [826, 533]])
+    image = results[0].plot(labels=False)
+    cv2.imshow("SAM", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
