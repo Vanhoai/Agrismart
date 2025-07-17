396bbc8fd5584efcce7b18d4b80381f7f43b94c1
Hinsun <vanhoai.adv@gmail.com>
2025-07-17 15:33:05 +0700

    feat: update YOLO model training setup, add new model file, and update dependencies

--- src/training/__init__.py
+++ src/training/__init__.py
@@ -1,20 +1,12 @@
+import os
 import supervision as sv
 from ultralytics import YOLO, SAM
 import cv2
 
-# source_path = "/Users/hinsun/Workspace/Software/Agrismart/assets/traffic.mp4"
-image_path = "/Users/hinsun/Workspace/Software/Agrismart/assets/traffic.jpg"
+root_directory = os.getcwd()
+dataset_directory = os.path.join(root_directory, "datasets", "rice-leaf-diseases")
+yaml_path = os.path.join(dataset_directory, "data.yaml")
 
 def main() -> None:
-    # (2160, 3840, 3)
-    # model = YOLO("yolo11x.pt")
-
-    # model.info()
-    # model.predict(source=image_path, imgsz=3840, conf=0.25, show=True, save=True)
-
-    model = SAM("sam2.1_b.pt")
-    results = model(image_path, points=[[992, 1630], [1379, 1325], [1397, 1200], [1450, 1024], [1334, 883], [1118, 667], [1055, 707], [844, 634], [725, 601], [1015, 558], [401, 498], [826, 533]])
-    image = results[0].plot(labels=False)
-    cv2.imshow("SAM", image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    model = YOLO("yolo11n.pt")
+    results = model.train(data=yaml_path, epochs=100, imgsz=640, batch=1, device="mps")
