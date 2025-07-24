de8d32cdcdab8fcf32f2799dae1633da96af1f1c
Hinsun <vanhoai.adv@gmail.com>
2025-07-24 12:26:26 +0700

    feat: refactor post and account augmenters for improved structure and functionality, add base augmentation class

--- packages/infrastructure/src/infrastructure/augmenters/__init__.py
+++ packages/infrastructure/src/infrastructure/augmenters/__init__.py
@@ -1,5 +1,6 @@
 import os
 from .account import AccountAugmenter
+from .post import PostAugmenter
 
 from core.database import Database, CollectionName
 
@@ -10,11 +11,21 @@ class Augmenter:
             raise FileNotFoundError(f"Directory {directory} does not exist.")
 
         account_csv = os.path.join(directory, "accounts.csv")
-        account_collection = database.get_collection(CollectionName.ACCOUNTS)
-        self.account_augmenter = AccountAugmenter(account_csv, account_collection)
+        post_csv = os.path.join(directory, "posts.csv")
+
+        self.account_collection = database.get_collection(CollectionName.ACCOUNTS)
+        self.post_collection = database.get_collection(CollectionName.POSTS)
+
+        self.account_augmenter = AccountAugmenter(account_csv, self.account_collection, CollectionName.ACCOUNTS)
+        self.post_augmenter = PostAugmenter(post_csv, self.post_collection, CollectionName.POSTS)
 
     async def monitor(self):
         await self.account_augmenter.monitor()
+
+        accounts = await self.account_collection.find({}).to_list(length=None)
+        account_ids = [str(account["_id"]) for account in accounts]
+        self.post_augmenter.provide_accounts(account_ids)
+        await self.post_augmenter.monitor()
 
 
 __all__ = ["Augmenter"]
